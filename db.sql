978a2617034d357880a9a498aa0c22962c734f33
Lautaro Andres Almiron <laualmiron2005@gmail.com>
2024-09-09 03:05:25 -0300

    Update db.sql
    cambio de signo al correcto

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE IF NOT EXISTS `pweb`;
+CREATE DATABASE IF NOT EXISTS `pweb`; 
 USE `pweb`;
 
     CREATE TABLE IF NOT EXISTS `users` (
-        'user_id' INT AUTO_INCREMENT PRIMARY KEY,
-        'username' VARCHAR(255) NOT NULL UNIQUE,
-        'password' VARCHAR(255) NOT NULL
+        `user_id` INT AUTO_INCREMENT PRIMARY KEY,
+        `username` VARCHAR(255) NOT NULL UNIQUE,
+        `password` VARCHAR(255) NOT NULL
     );
